d3d94552367b73ab2958feb0e5a62b44eb65acbb
7RCZ3x2jizsGxH8 <aliaksandr.ihnatsiuk@innowise-group.com>
2021-09-06 13:13:01 +0300

    Add  functions : generation_points, coeff

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import numpy as np
-from src.numpy_function import combination
+from src.numpy_function import combination, generation_points, line, coeff
+import matplotlib.pyplot as plt
 
-y = np.arange(5, 10, 0.8)
-x = np.arange(0, 5, 0.9)
+x_min = -1
+x_max = 1
+N = 300
+k = -0.5
+b = 3
 
-d = combination(x, y)
+(x, y) = generation_points(-1,1,-0.5,2,300)
+(k_new, b_new) = coeff(x,y)
 
-print(d)
+
+print ("k =", k, "b =",b)
+print ("k_new =", round(k_new,2), "b_new =",round(b_new,2))
+plt.scatter(x, y)
+plt.plot(x, k*x + b,'r')
+plt.plot(x, k_new*x + b_new,'g')
+plt.show()
--- src/numpy_function.py
+++ src/numpy_function.py
@@ -1,5 +1,22 @@
+from math import pi, sqrt, e
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 def combination(x, y):
     return np.array(np.meshgrid(x, y)).T.reshape(-1, 2)
+
+def line(k, b, x):
+    plt.plot(x, k * x + b, 'r')
+
+
+def generation_points(x_min, x_max, k, b, N):
+    x = np.linspace(x_min, x_max, N)
+    y = k * x + b + np.random.normal(-1, 2, N)
+    plt.scatter(x, y)
+    return x,y
+
+def coeff(x,y):
+    coeff = np.polyfit(x,y,1)
+    k_new = np.polyfit(x,y,1)[0]
+    b_new = np.polyfit(x,y,1)[1]
+    return k_new, b_new
