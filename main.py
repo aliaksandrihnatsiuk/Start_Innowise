6381f39d77c820fb3c108030ea2c4e010aad79ba
7RCZ3x2jizsGxH8 <aliaksandr.ihnatsiuk@innowise-group.com>
2021-09-08 13:04:18 +0300

    Split into functions

--- main.py
+++ main.py
@@ -5,30 +5,46 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
-    N = 15
-    w0 = 2
-    w1 = 0.1
+    N = 50
+    w0 = 0.2
+    w1 = 0.2
     w2 = 0.5
-    W = np.array([w0, w1, w2]).reshape(3,1)
 
-    x0 = np.ones(N)
-    x1 = np.linspace(1,2,N)
-    x2= np.linspace(-1,2,N)
+    def W_creation(w0, w1, w2):
+        return np.array([w0, w1, w2]).reshape(3, 1)
 
-    X = np.array([x0, x1, x2]).T
-    Y = w0 + w1*x1 + x2*w2 + np.random.normal(0,1,N)
-    print ('Coefficients, w0, w1, w2')
-    print(coeff_3(X, Y))
+    def X_creation (N):
+        x0 = np.ones(N*N)
+        X1 = np.linspace(1,2,N) + np.random.normal(0, 0.1, N)
+        X2 = np.linspace(-1,2,N) + np.random.normal(0, 6, N)
+        X = np.array(np.meshgrid(X1,X2)).T.reshape(-1,2)
+        X = np.hstack([x0[:,np.newaxis], X])
+        return X
+
+    def Y_creation(X, W):
+        Y_data = X.dot(W).reshape(N * N) + np.random.normal(0, 2, N * N)
+        return Y_data
+
+    def z_function(X, W):
+        return np.sum(X*W.T, axis=1)
+
+
+    W = W_creation(w0, w1, w2)
+    X = X_creation(N)
+    Y_data = Y_creation(X,W)
+
+    print('Coefficients, w0, w1, w2')
+    print(coeff_3(X, Y_data))
+
     # Draw Plot3D
-
-    def y_function(x1, x2):
-        return w0 + w1 * x1 + x2 * w2
-
-    X1, X2 = np.meshgrid(x1, x2)
-    Y = y_function(X1, X2)
+    Z = z_function(X,W)
     ax = plt.axes(projection='3d')
-    ax.plot_surface(X1, X2, Y)
-    ax.scatter(X1, X2, Y, c='r')
+    ax.scatter(X[:,1],X[:,2], Z[:,np.newaxis])
+    ax.scatter(X[:, 1], X[:, 2], Y_data[:, np.newaxis]) #after change on plot surface
+
+    ax.set_xlabel('x1')
+    ax.set_ylabel('x2')
+    ax.set_zlabel('y')
     plt.show()
 
 
